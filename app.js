7f04cfa6f204840bce983053a4ac08c6b8a49ff5
creationPrimal <143657193+creationPrimal@users.noreply.github.com>
2024-01-29 12:09:35 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ menu.addEventListener("click", ()=>{
 menuclose.addEventListener("click", ()=>{
     menudiv.style.display = 'none'
 })
+
+
+  // Get all thumbnail images
+  var thumbnails = document.querySelectorAll('.thumbnail');
+
+  // Add click event listener to each thumbnail
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', function() {
+      openModal(thumbnail.src);
+    });
+  });
+
+  function openModal(imageSrc) {
+    var modal = document.getElementById('myModal');
+    var modalImg = modal.querySelector('.modal-content');
+    
+    modalImg.src = imageSrc;
+    modal.style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
